f580a6813c176b6e0a5878ed51a4375fe8790b82
Jose9820 <pliegogomezj@gmail.com>
2018-10-10 20:37:05 -0500

    en esta version se utiliza un arreglo para los valores

--- Arboles.cpp
+++ Arboles.cpp
@@ -15,11 +15,11 @@ struct Nodo{
 //prototipo de funcion
 void menu();
 Nodo *crearNodo(int);
-void insertarNodo(Nodo *&, int n);
+void insertarNodo(Nodo *&, int);
 Nodo *arbol = NULL;
 
 int main(){
-    menu();
+    menu();  
 	return 0;
 }
 //funcion para crear un nuevo nodo
@@ -38,7 +38,7 @@ void insertarNodo(Nodo *&arbol, int n){
  	}else{//si el arbol tiene un noda mas
      	int valorRaiz = arbol->dato;//obtenemos el valor de la raiz
      	if (n == valorRaiz) {
-     		cout << "El elemento ya existe";
+     		;
 		 }
      	else if(n < valorRaiz){//si el elemento es menor a la raiz, insertamos en izquierda
          	insertarNodo(arbol->izquierdo, n);
@@ -52,34 +52,43 @@ void mostrarArbol(Nodo *&arbol, int cont){
     if(arbol == NULL){//si el arbol esta vacio
         return;
     }else{//si el arbol tiene un nodo o mas
-        mostrarArbol(arbol->derecho, cont+1);
-        for(int i=0; i<cont;i++){
+         mostrarArbol(arbol->derecho, cont+1);
+         for(int i=0; i<cont;i++){
             cout << "   ";
-        }
-        cout << arbol->dato << endl;
-        mostrarArbol(arbol->izquierdo, cont+1);
+         }
+         cout << arbol->dato << endl;
+         mostrarArbol(arbol->izquierdo, cont+1);
     }
 }
 
 void menu(){
-    int dato, opcion, contador=0;
-    do{
-        cout << "\nMenu" << endl;
-        cout << "1.- insertar nuevo nodo" << endl;
-        cout << "2.- mostrarArbol" << endl;
+ 	int dato, opcion, contador=0, n, datos[1000];
+ 	bool bandera=true;
+ 	do{
+ 		if (bandera == true) {
+ 			cout << "\nCantidad de datos: ";
+	     	cin >> n;
+	     	cout << endl;
+	     	for (int i = 0; i < n; i++) {
+         		cout << "Digite un numero: ";
+             	cin >> datos[i];
+             	insertarNodo(*&arbol, datos[i]);    	    
+		 }
+		 cout << endl;
+	}
+ 		cout << "1.- Agregar mas datos" << endl;
+        cout << "2.- Mostrar Arbol" << endl;
         cout << "3.- Salir" << endl;
         cin>>opcion;
         switch(opcion){
             case 1:
-                cout << "Digite un numero" << endl;
-                cin >> dato;
-                insertarNodo(arbol, dato);
-                cout << endl;
-                system("pause");
+            	system("pause");
+            	bandera = true;
                 break;
             case 2:
                 cout << "\nMostrado Arbol completo" << endl;
                 mostrarArbol(arbol, contador);
+                bandera = false;
                 system("pause");
                 break;
         }
