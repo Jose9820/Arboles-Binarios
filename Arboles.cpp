ee80b0246821ff6bf1d5784dfb825c7ab96238a2
Jose9820 <pliegogomezj@gmail.com>
2018-10-11 18:55:44 -0500

    no parece ser necesario el arreglo para guardar los valores introducidos por el usuario

--- Arboles.cpp
+++ Arboles.cpp
@@ -1,40 +1,42 @@
-//no duplicados
-//arreglo para los valores
 //imprimir de forma vertical
 
 #include <iostream>
 #include <stdlib.h>
 #include <conio.h>
+
 using namespace std;
 
 struct Nodo{
-    int dato;
-    Nodo *izquierdo;
-    Nodo *derecho;
+ 	int dato;
+ 	Nodo *izquierdo;
+ 	Nodo *derecho;
 };
+
 //prototipo de funcion
 void menu();
 Nodo *crearNodo(int);
-void insertarNodo(Nodo *&, int);
+void insertarNodo(Nodo *&, int []);
 Nodo *arbol = NULL;
 
 int main(){
-    menu();  
-	return 0;
+ 	menu();  
+ 	return 0;
 }
+
 //funcion para crear un nuevo nodo
 Nodo *crearNodo (int n){
-    Nodo *inicio = new Nodo ();
-    inicio->dato = n;
-    inicio->izquierdo = NULL;
-    inicio->derecho = NULL;
-    return inicio;
+ 	Nodo *inicio = new Nodo ();
+ 	inicio->dato = n;
+ 	inicio->izquierdo = NULL;
+ 	inicio->derecho = NULL;
+ 	return inicio;
 }
+
 //insertar elementos en el arbol
 void insertarNodo(Nodo *&arbol, int n){
  	if(arbol == NULL){//si el arbol esta vacio
-        Nodo *inicio = crearNodo(n);
-        arbol = inicio;
+     	Nodo *inicio = crearNodo(n);
+   	 	arbol = inicio;
  	}else{//si el arbol tiene un noda mas
      	int valorRaiz = arbol->dato;//obtenemos el valor de la raiz
      	if (n == valorRaiz) {
@@ -42,27 +44,27 @@ void insertarNodo(Nodo *&arbol, int n){
 		 }
      	else if(n < valorRaiz){//si el elemento es menor a la raiz, insertamos en izquierda
          	insertarNodo(arbol->izquierdo, n);
-     	}else{//si el elemento es mayor a la izq, insertamos en derecha
+       	}else{//si el elemento es mayor a la izq, insertamos en derecha
          	insertarNodo(arbol->derecho, n);
          }
-    }
+ 	 }
 }
 
 void mostrarArbol(Nodo *&arbol, int cont){
-    if(arbol == NULL){//si el arbol esta vacio
-        return;
-    }else{//si el arbol tiene un nodo o mas
+ 	if(arbol == NULL){//si el arbol esta vacio
+ 		return;
+ 	}else{//si el arbol tiene un nodo o mas
          mostrarArbol(arbol->derecho, cont+1);
          for(int i=0; i<cont;i++){
-            cout << "   ";
+         	cout << "   ";
          }
-         cout << arbol->dato << endl;
-         mostrarArbol(arbol->izquierdo, cont+1);
-    }
+     	cout << arbol->dato << endl;
+     	mostrarArbol(arbol->izquierdo, cont+1);
+     }
 }
 
 void menu(){
- 	int dato, opcion, contador=0, n, datos[1000];
+ 	int dato, opcion, contador=0, n;
  	bool bandera=true;
  	do{
  		if (bandera == true) {
@@ -71,27 +73,34 @@ void menu(){
 	     	cout << endl;
 	     	for (int i = 0; i < n; i++) {
          		cout << "Digite un numero: ";
-             	cin >> datos[i];
-             	insertarNodo(*&arbol, datos[i]);    	    
-		 }
-		 cout << endl;
-	}
+             	cin >> dato;
+				 insertarNodo(*&arbol, dato);  	    
+			 }
+	 		cout << endl;
+ 		 }
  		cout << "1.- Agregar mas datos" << endl;
-        cout << "2.- Mostrar Arbol" << endl;
-        cout << "3.- Salir" << endl;
-        cin>>opcion;
-        switch(opcion){
-            case 1:
-            	system("pause");
-            	bandera = true;
-                break;
-            case 2:
-                cout << "\nMostrado Arbol completo" << endl;
-                mostrarArbol(arbol, contador);
-                bandera = false;
-                system("pause");
-                break;
-        }
-        system("cls");
-    }while(opcion != 3);
-}
+     	cout << "2.- Mostrar Arbol" << endl;
+     	cout << "3.- Salir" << endl;
+     	cin>>opcion;
+	 	switch(opcion){
+         	case 1:
+             	bandera = true;
+             	break;
+         	case 2:
+             	cout << "\nMostrado Arbol completo:\n" << endl;
+             	mostrarArbol(arbol, contador);
+             	bandera = false;
+         		break;
+            case 3:
+             	return;
+             	break;
+            default :
+             	cout << "\nError: Opcion invalida. Reintente...\n";
+             	bandera = false;
+             	break;
+     	 }
+     	cout << endl;
+     	system("pause");
+     	system("cls");
+ 	 }while(opcion != 3);
+ }
